306a272cc8193cef61b063aab03c37f6200ec87b
Anastasia Boyko <taboyko24@gmail.com>
2021-05-19 00:24:07 +0300

    DONE

--- Downloads/goit-react-hw-08-phonebook-main/src/components/ContactsAppContainer/ContactsAppContainer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Container.module.css';
-
-const ContactsAppContainer = ({ children }) => (
-  <div className={styles.Container}>{children}</div>
-);
-
-export default ContactsAppContainer;
